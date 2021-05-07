baa24be2e0ece9099ce8e1a737c851030451508d
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2021-05-07 18:16:07 +0430

    remove location plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:wifi_plugin/wifi_plugin.dart';
-import 'package:location/location.dart';
-import 'package:wifi_plugin_example/util.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,37 +12,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  String ssid = '';
-  Location location = new Location();
-
-  bool _serviceEnabled;
-  PermissionStatus _permissionGranted;
-
+  
   @override
   void initState() {
     super.initState();
-    _requestPermission();
-  }
-
-  _requestPermission() async {
-
-    _serviceEnabled = await location.serviceEnabled();
-    print(_serviceEnabled);
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
   }
 
   @override
@@ -77,13 +48,7 @@ class _MyAppState extends State<MyApp> {
       accessPoints.forEach((key, value) {
         String bssid = key;
         List<int> rssiValues = List.of(value)?.cast<int>();
-        if(rssiValues.length >= 8) {
-          print("Initial Size: " + rssiValues.length.toString());
-          rssiValues = Util().makeList(rssiValues, 7);
-          int avg = Util().calculateMean(rssiValues);
-          print(rssiValues);
-          print(avg);
-        }
+        print(rssiValues);
       });
     });
 
@@ -94,12 +59,11 @@ class _MyAppState extends State<MyApp> {
     List<dynamic> wifiList = await Wifi.wifiScanner;
     setState(() {
       wifiList.forEach((wifi) {
-         ssid = wifi['SSID'];
+        String ssid = wifi['SSID'];
         String bssid = wifi['BSSID'];
         int rssi = int.parse(wifi['RSSI']);
         int freq = int.parse(wifi['frequency']);
         int channel = int.parse(wifi['channel']);
-         print(ssid);
       });
     });
   }
